5cdc88f7a9f5c0fa0df8204a24d5e6ccfb4580e6
blockbusterkit6 <kitcovenant@gmail.com>
2024-01-30 10:18:02 -0600

    modified:   income-divider/src/main.rs modified:   income-divider/ui/appwindow.slint

--- income-divider/src/main.rs
+++ income-divider/src/main.rs
@@ -1,15 +1,26 @@
 slint::include_modules!();
 
-fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
+const TAXPER: f64 = 0.30;
+const O: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPEXPER: f64 = 0.10;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
+fn main() -> Result<(), slint::PlatformError> {
+    let ui:AppWindow = AppWindow::new()?;
+
+    let ui_handle: slint::Weak<AppWindow> = ui.as_weak();
+    ui.on_divide_income(move |string| {
+        let ui = ui_handle.unwrap();
+        let num: f64 = string.trim().parse().unwrap();
+        let tax: f64 = num * TAXPER;
+        let owner: f64 = num * O;
+        let profit: f64 = num * PROFITPER;
+        let opex: f64 = num * OPEXPER;
+        let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpEx {:.2}", tax, owner, profit, opex);
+        ui.set_results(result.into());
     });
+
+
 
     ui.run()
 }
